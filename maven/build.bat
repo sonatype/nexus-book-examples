cb63b6ed2f3d3aac91c170557d1e8bd89f751ff2
Manfred Moser <manfred@simpligility.com>
2012-12-27 17:00:04 -0800

    adding examples from old guide before updating

---
+++ maven/build.bat
@@ -0,0 +1,30 @@
+@echo off
+if "%BUILD_DEBUG_ECHO%" == "on" echo on
+
+if "%OS%" == "Windows_NT" setlocal enableextensions
+
+:begin
+
+rem Detect base directory, this will return a value suffixed with \
+set "dirname=%~dp0"
+if "%dirname%" == "" set "dirname=.\"
+
+rem Force evalguide maven to be used
+set "M2_HOME=%dirname%maven"
+
+rem Avoid pulling in RC which might break things
+set "MAVEN_SKIP_RC=true"
+
+rem Have to append "." to basedir to avoid \" causing mvn to not read arguments
+rem (not sure why this is needed though, but it is)
+set "basedir=%dirname%."
+
+:execute
+
+rem Run maven with custom settings
+call "%M2_HOME%\bin\mvn.bat" "-Devalguide.basedir=%basedir%" -s "%dirname%settings.xml" %*
+
+:end
+
+if "%OS%" == "Windows_NT" endlocal
+

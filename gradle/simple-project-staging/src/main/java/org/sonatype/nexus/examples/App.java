26ff99a72bc264e643249b43325a84ee4d8db531
Manfred Moser <manfred@simpligility.com>
2014-07-04 15:41:19 -0700

    adding new staging example project

---
+++ gradle/simple-project-staging/src/main/java/org/sonatype/nexus/examples/App.java
@@ -0,0 +1,13 @@
+package org.sonatype.nexus.examples;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}

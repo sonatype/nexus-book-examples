a3f3ffaa8520ef204da5b1140bd33821f0f65c3a
Manfred Moser <manfred@simpligility.com>
2016-02-19 11:34:03 -0800

    fixed script to work again as prior to refactor ;-)

--- scripting/complex-script/security.groovy
+++ scripting/complex-script/security.groovy
@@ -19,9 +19,10 @@ security.addUser("jane.doe", "Jane", "Doe", "jane.doe@example.com", true, "chang
 def devPrivileges = new HashSet(["nx-healthcheck-read", "nx-healthcheck-summary-read"])
 def anoRole = new HashSet(["nx-anonymous"])
 // add roles that uses the built in nx-anonymous role as a basis and adds more privileges
-def devRole = security.addRole("developer", "Developer", "User with privileges to allow read access to repo content and healthcheck", devPrivileges, anoRole)
+security.addRole("developer", "Developer", "User with privileges to allow read access to repo content and healtcheck", devPrivileges, anoRole)
 // use the new role to create a user 
-security.addUser("john.doe", "John", "Doe", "john.doe@example.com", true, "changMe456", devRole)
+def devRoles = ["developer"]
+security.addUser("john.doe", "John", "Doe", "john.doe@example.com", true, "changMe456", devRoles)
 
 
 //
@@ -29,7 +30,8 @@ security.addUser("john.doe", "John", "Doe", "john.doe@example.com", true, "chang
 //
 // privileges with pattern * to allow any format, browse and read are already part of nx-anonymous
 def depPrivileges = new HashSet(["nx-repository-view-*-*-add", "nx-repository-view-*-*-edit"])
-def devRole = new HashSet(["developer"]) 
+def roles = new HashSet(["developer"])
 // add roles that uses the developer role as a basis and adds more privileges
-def depRole = security.addRole("deployer", "Deployer", "User with privileges to allow deployment all repositories", depPrivileges, devRole)
-security.addUser("jenkins", "Leeroy", "Jenkins", "leeroy.jenkins@example.com", true, "changMe789", depRole)
+security.addRole("deployer", "Deployer", "User with privileges to allow deployment all repositories", depPrivileges, roles)
+def depRoles = ["deployer"]
+security.addUser("jenkins", "Leeroy", "Jenkins", "leeroy.jenkins@example.com", true, "changMe789", depRoles)

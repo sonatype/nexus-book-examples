2334a814a3c372c1543d3b2fbc8d5f985b4488a2
Kelly Robinson <krobinson@sonatype.com>
2016-02-25 10:31:31 -0800

    - add full set of dependencies required for full code-completion to pom - use single quotes instead of GStrings - remove use of HashSet as api has been updated to not require them

--- scripting/complex-script/dockerRepositories.groovy
+++ scripting/complex-script/dockerRepositories.groovy
@@ -1,13 +1,13 @@
 import org.sonatype.nexus.blobstore.api.BlobStoreManager
 
 // create hosted repo and expose via https to allow deployments
-repository.createDockerHosted("docker-internal", null, 18444) 
+repository.createDockerHosted('docker-internal', null, 18444) 
 
 // create proxy repo of Docker Hub and enable v1 to get search to work
 // no ports since access is only indirectly via group
-repository.createDockerProxy("docker-hub",                   // name
-                             "https://registry-1.docker.io", // remoteUrl
-                             "HUB",                          // indexType
+repository.createDockerProxy('docker-hub',                   // name
+                             'https://registry-1.docker.io', // remoteUrl
+                             'HUB',                          // indexType
                              null,                           // indexUrl
                              null,                           // httpPort
                              null,                           // httpsPort
@@ -17,6 +17,6 @@ repository.createDockerProxy("docker-hub",                   // name
                              )
 
 // create group and allow access via https
-repository.createDockerGroup("docker-all", null, 18443, true, "docker-internal", "docker-hub")
+repository.createDockerGroup('docker-all', null, 18443, true, 'docker-internal', 'docker-hub')
 
 
--- scripting/complex-script/security.groovy
+++ scripting/complex-script/security.groovy
@@ -9,29 +9,29 @@ security.setAnonymousAccess(false)
 //
 // Create new admin user
 //
-def adminRole = ["nx-admin"] 
-security.addUser("jane.doe", "Jane", "Doe", "jane.doe@example.com", true, "changMe123", adminRole)
+def adminRole = ['nx-admin'] 
+security.addUser('jane.doe', 'Jane', 'Doe', 'jane.doe@example.com', true, 'changMe123', adminRole)
 
 
 //
 // Create a new role that allows a user same access as anonymous and adds healtchcheck access
 //
-def devPrivileges = new HashSet(["nx-healthcheck-read", "nx-healthcheck-summary-read"])
-def anoRole = new HashSet(["nx-anonymous"])
+def devPrivileges = ['nx-healthcheck-read', 'nx-healthcheck-summary-read']
+def anoRole = ['nx-anonymous']
 // add roles that uses the built in nx-anonymous role as a basis and adds more privileges
-security.addRole("developer", "Developer", "User with privileges to allow read access to repo content and healtcheck", devPrivileges, anoRole)
+security.addRole('developer', 'Developer', 'User with privileges to allow read access to repo content and healtcheck', devPrivileges, anoRole)
 // use the new role to create a user 
-def devRoles = ["developer"]
-security.addUser("john.doe", "John", "Doe", "john.doe@example.com", true, "changMe456", devRoles)
+def devRoles = ['developer']
+security.addUser('john.doe', 'John', 'Doe', 'john.doe@example.com', true, 'changMe456', devRoles)
 
 
 //
 // Create new role that allows deployment and create a user to be used on a CI server
 //
 // privileges with pattern * to allow any format, browse and read are already part of nx-anonymous
-def depPrivileges = new HashSet(["nx-repository-view-*-*-add", "nx-repository-view-*-*-edit"])
-def roles = new HashSet(["developer"])
+def depPrivileges = new HashSet(['nx-repository-view-*-*-add', 'nx-repository-view-*-*-edit'])
+def roles = new HashSet(['developer'])
 // add roles that uses the developer role as a basis and adds more privileges
-security.addRole("deployer", "Deployer", "User with privileges to allow deployment all repositories", depPrivileges, roles)
-def depRoles = ["deployer"]
-security.addUser("jenkins", "Leeroy", "Jenkins", "leeroy.jenkins@example.com", true, "changMe789", depRoles)
+security.addRole('deployer', 'Deployer', 'User with privileges to allow deployment all repositories', depPrivileges, roles)
+def depRoles = ['deployer']
+security.addUser('jenkins', 'Leeroy', 'Jenkins', 'leeroy.jenkins@example.com', true, 'changMe789', depRoles)

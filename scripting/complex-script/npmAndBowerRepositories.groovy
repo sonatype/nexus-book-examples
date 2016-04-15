189023b232584b83ba5a55f49bf6230f5e085479
Kelly Robinson <krobinson@sonatype.com>
2016-04-14 17:18:03 -0700

    NEXUS-10061 - Integration API return values - add examples of creating JSON/XML responses from scripts

--- scripting/complex-script/npmAndBowerRepositories.groovy
+++ scripting/complex-script/npmAndBowerRepositories.groovy
@@ -1,15 +1,38 @@
-repository.createNpmProxy('npmjs-org', 'https://registry.npmjs.org')
+import groovy.xml.MarkupBuilder
+def newRepositories = []
+newRepositories << repository.createNpmProxy('npmjs-org2', 'https://registry.npmjs.org')
 
-repository.createNpmHosted('npm-internal')
+newRepositories << repository.createNpmHosted('npm-internal2')
 
 def npmMembers = ['npmjs-org', 'npm-internal' ]
-repository.createNpmGroup('npm-all', npmMembers)
+newRepositories << repository.createNpmGroup('npm-all2', npmMembers)
 
-repository.createBowerProxy('bower-io', 'http://bower.herokuapp.com')
+newRepositories << repository.createBowerProxy('bower-io2', 'http://bower.herokuapp.com')
 
-repository.createBowerHosted('bower-internal')
+newRepositories << repository.createBowerHosted('bower-internal2')
 
 def bowerMembers = ['bower-io', 'bower-internal']
-repository.createBowerGroup('bower-all', bowerMembers)
+newRepositories << repository.createBowerGroup('bower-all2', bowerMembers)
 
 log.info('Script npmAndBowerRepositories completed successfully')
+
+// build up an XML response containing the urls for newly created repositories
+def writer = new StringWriter()
+def xml = new MarkupBuilder(writer)
+xml.repositories() {
+  newRepositories.each { repo ->
+    repository(name: repo.name, url: repo.url)    
+  }  
+}
+return writer.toString()
+
+// output will be like:
+
+//<repositories>
+//  <repository name='npmjs-org2' url='http://localhost:8081/repository/npmjs-org2' />
+//  <repository name='npm-internal2' url='http://localhost:8081/repository/npm-internal2' />
+//  <repository name='npm-all2' url='http://localhost:8081/repository/npm-all2' />
+//  <repository name='bower-io2' url='http://localhost:8081/repository/bower-io2' />
+//  <repository name='bower-internal2' url='http://localhost:8081/repository/bower-internal2' />
+//  <repository name='bower-all2' url='http://localhost:8081/repository/bower-all2' />
+//</repositories>
--- scripting/nexus-script-example/src/main/groovy/maven.groovy
+++ scripting/nexus-script-example/src/main/groovy/maven.groovy
@@ -3,7 +3,7 @@
 import org.sonatype.nexus.blobstore.api.BlobStoreManager
 import org.sonatype.nexus.repository.storage.WritePolicy
 
-repository.createMavenHosted("private")
+repository.createMavenHosted('private')
 
-repository.createMavenHosted("private-again", BlobStoreManager.DEFAULT_BLOBSTORE_NAME, VersionPolicy.RELEASE,
+repository.createMavenHosted('private-again', BlobStoreManager.DEFAULT_BLOBSTORE_NAME, VersionPolicy.RELEASE,
         WritePolicy.ALLOW_ONCE, LayoutPolicy.STRICT)

5a3dfa6f21e9332a8c594d1e786205a61236cfd0
Kelly Robinson <krobinson@sonatype.com>
2016-04-18 11:18:41 -0700

    NEXUS-10061 - Integration API return values - remove stoopid suffix attached during local testing

--- scripting/complex-script/npmAndBowerRepositories.groovy
+++ scripting/complex-script/npmAndBowerRepositories.groovy
@@ -1,18 +1,18 @@
 import groovy.xml.MarkupBuilder
 def newRepositories = []
-newRepositories << repository.createNpmProxy('npmjs-org2', 'https://registry.npmjs.org')
+newRepositories << repository.createNpmProxy('npmjs-org', 'https://registry.npmjs.org')
 
-newRepositories << repository.createNpmHosted('npm-internal2')
+newRepositories << repository.createNpmHosted('npm-internal')
 
 def npmMembers = ['npmjs-org', 'npm-internal' ]
-newRepositories << repository.createNpmGroup('npm-all2', npmMembers)
+newRepositories << repository.createNpmGroup('npm-all', npmMembers)
 
-newRepositories << repository.createBowerProxy('bower-io2', 'http://bower.herokuapp.com')
+newRepositories << repository.createBowerProxy('bower-io', 'http://bower.herokuapp.com')
 
-newRepositories << repository.createBowerHosted('bower-internal2')
+newRepositories << repository.createBowerHosted('bower-internal')
 
 def bowerMembers = ['bower-io', 'bower-internal']
-newRepositories << repository.createBowerGroup('bower-all2', bowerMembers)
+newRepositories << repository.createBowerGroup('bower-all', bowerMembers)
 
 log.info('Script npmAndBowerRepositories completed successfully')
 
@@ -29,10 +29,10 @@ return writer.toString()
 // output will be like:
 
 //<repositories>
-//  <repository name='npmjs-org2' url='http://localhost:8081/repository/npmjs-org2' />
-//  <repository name='npm-internal2' url='http://localhost:8081/repository/npm-internal2' />
-//  <repository name='npm-all2' url='http://localhost:8081/repository/npm-all2' />
-//  <repository name='bower-io2' url='http://localhost:8081/repository/bower-io2' />
-//  <repository name='bower-internal2' url='http://localhost:8081/repository/bower-internal2' />
-//  <repository name='bower-all2' url='http://localhost:8081/repository/bower-all2' />
+//  <repository name='npmjs-org' url='http://localhost:8081/repository/npmjs-org' />
+//  <repository name='npm-internal' url='http://localhost:8081/repository/npm-internal' />
+//  <repository name='npm-all' url='http://localhost:8081/repository/npm-all' />
+//  <repository name='bower-io' url='http://localhost:8081/repository/bower-io' />
+//  <repository name='bower-internal' url='http://localhost:8081/repository/bower-internal' />
+//  <repository name='bower-all' url='http://localhost:8081/repository/bower-all' />
 //</repositories>

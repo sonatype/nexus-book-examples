2a0434fe17b3155d9f52201ca99fb7debd086f2d
Manfred Moser <manfred@simpligility.com>
2016-05-27 15:30:57 -0700

    json and script to enable/disable anonymous

---
+++ scripting/nexus-script-example/src/main/json/setAnonymous.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+anonymous=$1
+
+printf "Setting Anonymous access to $anonymous\n\n"
+
+curl -v -X POST -u admin:admin123 --header "Content-Type: text/plain" "http://localhost:8081/service/siesta/rest/v1/script/anonymous/run" -d $anonymous

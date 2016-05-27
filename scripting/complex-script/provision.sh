8c921246a30a5bc1c34419bbf85ae9e9fc4a7ce9
Manfred Moser <manfred@simpligility.com>
2016-05-27 16:41:02 -0700

    improved logging

--- scripting/complex-script/provision.sh
+++ scripting/complex-script/provision.sh
@@ -18,9 +18,9 @@ function addAndRunScript {
   name=$1
   file=$2
   groovy -Dgroovy.grape.report.downloads=true addUpdatescript.groovy -u "$username" -p "$password" -n "$name" -f "$file" -h "$host"
-  printf "\nPublished $file as $name\n"
+  printf "\nPublished $file as $name\n\n"
   curl -v -X POST -u $username:$password --header "Content-Type: text/plain" "$host/service/siesta/rest/v1/script/$name/run"
-  printf "\nSuccessfully executed $name script\n"
+  printf "\nSuccessfully executed $name script\n\n\n"
 }
 
 printf "Provisioning Integration API Scripts Starting \n\n" 
@@ -32,4 +32,4 @@ addAndRunScript raw rawRepositories.groovy
 addAndRunScript security security.groovy
 addAndRunScript core core.groovy
 
-printf "Provisioning Scripts Completed" 
+printf "\nProvisioning Scripts Completed\n\n"

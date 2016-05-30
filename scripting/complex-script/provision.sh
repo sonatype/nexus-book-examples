ad7c459078a93a95025e872b503daa3d44ee20ef
Manfred Moser <manfred@simpligility.com>
2016-05-30 10:19:08 -0700

    adding grapeConfig that points to central to avoid jcenter issues

--- scripting/complex-script/provision.sh
+++ scripting/complex-script/provision.sh
@@ -14,10 +14,12 @@ password=admin123
 # add the context if you are not using the root context
 host=http://localhost:8081
 
+# add a script to the repository manager and run it
 function addAndRunScript {
   name=$1
   file=$2
-  groovy -Dgroovy.grape.report.downloads=true addUpdatescript.groovy -u "$username" -p "$password" -n "$name" -f "$file" -h "$host"
+  # using grape config that points to local Maven repo and Central Repository , default grape config fails on some downloads although artifacts are in Central
+  groovy -Dgroovy.grape.report.downloads=true -Dgrape.config=grapeConfig.xml addUpdatescript.groovy -u "$username" -p "$password" -n "$name" -f "$file" -h "$host"
   printf "\nPublished $file as $name\n\n"
   curl -v -X POST -u $username:$password --header "Content-Type: text/plain" "$host/service/siesta/rest/v1/script/$name/run"
   printf "\nSuccessfully executed $name script\n\n\n"
